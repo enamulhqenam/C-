8ca62f6480e2bebf5c05e10c33f2b8c3cfef468a
Bin Sohorab <49332930+enamulhqenam@users.noreply.github.com>
2024-02-01 14:54:35 +0600

    Add files via upload

---
+++ InharitenceMultilavle.cpp
@@ -0,0 +1,39 @@
+
+#include<iostream>
+
+using namespace std;
+
+class Animal
+{
+
+public :
+    void eat()
+    {
+
+        cout << "Eation" << endl;
+    }
+};
+
+class Dog:public Animal{
+public:
+    void bark(){
+        cout <<"Barking..." <<endl;
+    }
+};
+
+class BabyDog:public Dog{
+public:
+    void weep(){
+        cout << "Weeping ...." << endl;
+    }
+};
+
+int main(){
+
+    BabyDog d1;
+    d1.eat();
+    d1.bark();
+    d1.weep();
+
+    return 0;
+}
